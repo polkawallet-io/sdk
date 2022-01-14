4486d711a25a8a7d42111145cc5040ecb0a1f9e2
shawn <dhryxy@gmail.com>
2022-01-14 15:52:24 +0800

    update plugin.appUtils pageRouteParams

--- lib/utils/app.dart
+++ lib/utils/app.dart
@@ -1,4 +1,12 @@
 class AppUtils {
   /// The App will set this function for plugins switch between each other
-  Future<void> Function(String network, {String? pageRoute})? switchNetwork;
+  Future<void> Function(String network, {PageRouteParams? pageRoute})?
+      switchNetwork;
+}
+
+class PageRouteParams {
+  PageRouteParams(this.path, {this.args});
+
+  final String path;
+  final Map? args;
 }
