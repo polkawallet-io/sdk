bde45d34ccb4c52c9be00e8af2729dcf26f74eae
gxhx <gxhx2006@foxmail.com>
2022-08-09 18:45:04 +0800

    check server connection

--- lib/service/localServer.dart
+++ lib/service/localServer.dart
@@ -1,9 +1,25 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 ///Single instance
 class LocalServer {
   final InAppLocalhostServer? _localhostServer;
   Future<void> startLocalServer() async {
+    if (_localhostServer?.isRunning() == true) {
+      HttpClient client = HttpClient();
+      HttpClientRequest? request;
+      try {
+        request = await client.getUrl(Uri.parse('http://localhost:8080/'));
+      } catch (error) {
+        debugPrint(error.toString());
+      }
+      final response = await request?.close();
+      client.close();
+      if (response?.statusCode == 200) {
+        return;
+      }
+    }
     await _localhostServer?.close();
     await _localhostServer?.start();
   }
