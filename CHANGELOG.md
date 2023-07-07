68f64f2a42625bde5bc291344d4dea41bcf77009
CryptoCatty <71962605+CryptoCatty@users.noreply.github.com>
2023-07-07 20:48:41 +0800

    release 0.5.4+4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [0.5.4+4]
+20230707
+* bump polkawallet/bridge 0.1.5-4
+* bump polkadot-js/api v10.9.1
+
 ## [0.5.4+3]
 20230620
 * settings.getNetworkProperties() toJSON fix.
--- js_api/src/service/bridge.ts
+++ js_api/src/service/bridge.ts
@@ -11,13 +11,10 @@ import { CrabAdapter } from "@polkawallet/bridge/adapters/darwinia";
 import { IntegriteeAdapter } from "@polkawallet/bridge/adapters/integritee";
 import { QuartzAdapter, UniqueAdapter } from "@polkawallet/bridge/adapters/unique";
 import { KintsugiAdapter, InterlayAdapter } from "@polkawallet/bridge/adapters/interlay";
-import { KicoAdapter } from "@polkawallet/bridge/adapters/kico";
-import { PichiuAdapter } from "@polkawallet/bridge/adapters/kylin";
 import { TuringAdapter } from "@polkawallet/bridge/adapters/oak";
 import { ParallelAdapter, HeikoAdapter } from "@polkawallet/bridge/adapters/parallel";
 import { KhalaAdapter } from "@polkawallet/bridge/adapters/phala";
-import { BasiliskAdapter, HydraAdapter } from "@polkawallet/bridge/adapters/hydradx";
-import { ListenAdapter } from "@polkawallet/bridge/adapters/listen";
+import { BasiliskAdapter, HydraDxAdapter } from "@polkawallet/bridge/adapters/hydradx";
 import { MoonbeamAdapter, MoonriverAdapter } from "@polkawallet/bridge/adapters/moonbeam";
 import { Observable, firstValueFrom, combineLatest } from "rxjs";
 import { BaseCrossChainAdapter } from "@polkawallet/bridge/base-chain-adapter";
@@ -51,17 +48,17 @@ const availableAdapters: Record<string, BaseCrossChainAdapter> = {
   calamari: new CalamariAdapter(),
   crab: new CrabAdapter(),
   heiko: new HeikoAdapter(),
-  hydra: new HydraAdapter(),
+  hydradx: new HydraDxAdapter(),
   integritee: new IntegriteeAdapter(),
   interlay: new InterlayAdapter(),
   khala: new KhalaAdapter(),
   kintsugi: new KintsugiAdapter(),
-  kico: new KicoAdapter(),
-  listen: new ListenAdapter(),
+  // kico: new KicoAdapter(),
+  // listen: new ListenAdapter(),
   moonbeam: new MoonbeamAdapter(),
   moonriver: new MoonriverAdapter(),
   parallel: new ParallelAdapter(),
-  pichiu: new PichiuAdapter(),
+  // pichiu: new PichiuAdapter(),
   quartz: new QuartzAdapter(),
   shadow: new ShadowAdapter(),
   shiden: new ShidenAdapter(),
